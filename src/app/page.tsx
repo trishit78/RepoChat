3c90004617d29c564776a0f80bdc0b9471c63b93
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-09-11 10:52:31 +0530

    added shadcn and clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   return (
+    <>
     <h1 className="text-red-400">Hello</h1>
-  )
+    <Button>CLick me</Button>
+
+    </>
+
+)
 }
